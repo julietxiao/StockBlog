8de2a9fc4d7d729dd5274708e89149a3f227623d
julietxiao <julietxiao99@qq.com>
2018-12-25 12:55:30 +0800

    修改数据库,增加了股票的新属性.

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -5,8 +5,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Stock
+# from .models import Stock_change
 
 class StockAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'industry', 'area')
+    list_display = ('name', 'code', 'industry', 'area', 'price_change', 'p_change','pe','gpr', 'npr')
+
 
 admin.site.register(Stock, StockAdmin)
+# admin.site.register(Stock_change)
