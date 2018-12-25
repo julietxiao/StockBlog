9fe03123bc8d12db3ac947fff2340300dc4b5a99
julietxiao <julietxiao99@qq.com>
2018-12-25 16:34:41 +0800

    修改首页,增加具体属性

--- mainsite/views.py
+++ mainsite/views.py
@@ -26,29 +26,30 @@ def homepage(request):
     #                       "</small><br></br>")
 
     now = datetime.now()
-    upper5, upper_five_stock = upper_top_five()
-    lower5, lower_five_stock = lower_top_five()
+    upper_five_stock = upper_top_five()
+    lower_five_stock = lower_top_five()
     html = template.render(locals())
 
     return HttpResponse(html)
 
 
 def upper_top_five():
-    upper_five_stock = Stock.objects.all()[1:6]
-    res = np.zeros((5, 3), dtype=np.int)
-    for i in range(5):
-        arr = np.random.random_integers(0, 50, 3)
-        res[i] = arr
-    return res, upper_five_stock
+    # upper_five_stock = Stock.objects.all()[1:6]
+    # 市盈率
+    data = Stock.objects.order_by('pe')
+    data = data[::-1]
+    upper_five_stock = data[0:5]
+
+    return upper_five_stock
 
 
 def lower_top_five():
-    lower_five_stock = Stock.objects.all()[10:16]
-    res = np.zeros((5, 3), dtype=np.int)
-    for i in range(5):
-        arr = np.random.random_integers(0, 50, 3)
-        res[i] = arr
-    return res, lower_five_stock
+    data = Stock.objects.order_by('npr')
+    data = data[::-1]
+
+    lower_five_stock = data[0:5]
+
+    return lower_five_stock
 
 
 def show_stock(request, stock_code):
